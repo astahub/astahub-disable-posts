d213df3af9987b73ee7ff828248e33cae80b2fe8
Haris Ainur Rozak <harisrozak@gmail.com>
2018-03-24 20:44:34 +0700

    updated plugin info

--- astahub-disable-posts.php
+++ astahub-disable-posts.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Astahub - Disable post type Post
-Plugin URI: 
+Plugin URI: https://github.com/astahub/astahub-disable-posts
 Description: Disable build in post type post and remove it from the menu.
 Author: harisrozak
-Author URI: 
+Author URI: https://github.com/harisrozak
 Version: 0.1
 Text Domain: astahub-disable-posts
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
